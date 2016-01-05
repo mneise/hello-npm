1b2533b53da9c5602cffb5c5f3a02695580bf82b
Maria Neise <mneise@liveops.com>
2016-01-06 11:26:19 +1300

    Update package.json.

--- scripts/release.clj
+++ scripts/release.clj
@@ -6,6 +6,8 @@
   (b/build "src"
     {:output-to "release/hello_npm.js"
      :output-dir "release"
-     :optimizations :advanced
-     :verbose true})
+     :optimizations :simple
+     :target :nodejs
+     :verbose true
+     :main 'hello-npm.core})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
